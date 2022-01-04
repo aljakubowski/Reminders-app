f5020ff8da6df8aa8025a800ae378b4a9a9a31cf
Alfred Jakubowski <alfredjakubowski@MacBook-Pro-Alfred.local>
2022-01-04 17:04:20 +0100

    add: get reminder by id and delete reminder functionalities

--- src/main/java/com/alja/Reminders/controller/RemindersController.java
+++ src/main/java/com/alja/Reminders/controller/RemindersController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/v1/reminders")
@@ -23,14 +24,25 @@ public class RemindersController {
         return this.remindersService.getReminders();
     }
 
-//    @GetMapping(path = "{id}")
-//    public Reminders getReminder(){
-//
-//    }
+    @GetMapping(path = "{id}")
+    public Optional getReminder(@PathVariable("id") Long id){
+        return this.remindersService.getReminder(id);
+    }
 
 
     @PostMapping
     public void addReminder(@RequestBody Reminders reminder){
         remindersService.addNewReminder(reminder);
     }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteReminder(@PathVariable("id") Long id){
+        remindersService.deleteReminder(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updateReminder(@PathVariable("id") Long id, @RequestBody Reminders reminder){
+        remindersService.updateReminder(id, reminder);
+    }
+
 }
--- src/main/java/com/alja/Reminders/service/RemindersService.java
+++ src/main/java/com/alja/Reminders/service/RemindersService.java
@@ -22,11 +22,53 @@ public class RemindersService {
         return remindersRepository.findAll();
     }
 
+//    public Reminders getReminder(Long id){
+//        if (checkIfReminderExists(id)){
+//            return remindersRepository.getById(id);
+//        }
+//        return null;
+//    }
+
+    public Optional getReminder(Long id){
+        Optional<Reminders> remindersOptional = remindersRepository.findById(id);
+        if (remindersOptional.isEmpty()){
+            throw new IllegalStateException("task with id: '" + id + "' does exists");
+        }
+        return remindersOptional;
+    }
+
+//    public Optional getEmployee(Long id){
+//
+//        Optional<Employee> employeeOptional = employeeRepository.findById(id);
+//        if (employeeOptional.isEmpty()){
+//            throw new IllegalStateException("Employee with id: " + id + " is not present");
+//        }
+//        return employeeOptional;
+//
+//    }
+
     public void addNewReminder(Reminders reminder) {
         Optional<Reminders> remindersOptional = remindersRepository.findReminderByTask(reminder.getTask());
         if (remindersOptional.isPresent()){
             throw new IllegalStateException("task already exists");
         }
         this.remindersRepository.save(reminder);
+    }
+
+    public void deleteReminder(Long id) {
+        if (checkIfReminderExists(id)){
+            this.remindersRepository.deleteById(id);
+        }
+    }
+
+    private boolean checkIfReminderExists(Long id){
+        if (!this.remindersRepository.existsById(id)) {
+            throw new IllegalStateException("Reminder with id: " + id + " does not exist");
+        }
+        return true;
+    }
+
+    public void updateReminder(Long id, Reminders reminder) {
+
     }
 }
